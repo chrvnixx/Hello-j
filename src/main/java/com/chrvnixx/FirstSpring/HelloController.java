0d15195f7327a76716686dc9869f717608c14a6a
chrvnixx <emmanueladekola91@gmail.com>
2026-06-19 20:44:35 +0100

    first api call in java

--- src/main/java/com/chrvnixx/FirstSpring/HelloController.java
+++ src/main/java/com/chrvnixx/FirstSpring/HelloController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
-    @GetMapping("/Hello")
+    @GetMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World!";
     }
 }
