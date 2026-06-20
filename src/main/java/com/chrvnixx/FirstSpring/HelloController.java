90de17a5de448842abf999efcb0fab729dadadfd
chrvnixx <emmanueladekola91@gmail.com>
2026-06-20 13:29:52 +0100

    post method

--- src/main/java/com/chrvnixx/FirstSpring/HelloController.java
+++ src/main/java/com/chrvnixx/FirstSpring/HelloController.java
@@ -2,18 +2,17 @@ package com.chrvnixx.FirstSpring;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String hello(){
+    public String helloGet(){
         return "Hello World!";
     }
 
     @PostMapping("/hello")
-    public HelloResponse helloPost(@RequestBody String name){
+    public HelloResponse helloPost(String name){
         return new HelloResponse("Hello" + name + "!");
-    }
+    };
 }
